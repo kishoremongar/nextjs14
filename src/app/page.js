62746202a8b1ee094bc2ffc871a53e02665fbdbc
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2024-02-25 23:06:21 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 export default function Home() {
-  const testtds = 1180;
+  const testtds = 111180;
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       hello world
