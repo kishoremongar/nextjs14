55a5c946293fa7bda41b6e04bb568a77f8a7fa02
Kishore Mongar <84139548+fushigina-Shinobi@users.noreply.github.com>
2024-02-25 22:05:13 +0530

    testing lint and husky

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 export default function Home() {
-  const testt = 8;
+  const testt = 18;
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       hello world
